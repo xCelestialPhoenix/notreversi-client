1bc5cc13ea2114697a3e8493dd335027d9e4103e
Phoenix <terenceng1996@gmail.com>
2021-12-20 16:32:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/notreversi'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/<app-name>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/notreversi/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
